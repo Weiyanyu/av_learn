7798c6c19bf030a89022336db6670e692ae85109
Yeonon <chinayeonon@gmail.com>
2023-05-03 08:12:10 -0700

    resample

--- device/src/device.cpp
+++ device/src/device.cpp
@@ -6,6 +6,7 @@ extern "C"
 #include <libavutil/avutil.h>
 #include <libavdevice/avdevice.h>
 #include <libavformat/avformat.h>
+#include <libswresample/swresample.h>
 #if defined(__cplusplus)
 }
 #endif
@@ -13,6 +14,8 @@ extern "C"
 #include "device.h"
 #include <iostream>
 #include <fstream>
+#include <stdio.h>
+
 
 Device::Device(const std::string& deviceName)
     :m_deviceName(deviceName)
@@ -32,11 +35,13 @@ Device::Device(const std::string& deviceName)
     {
         av_log(NULL, AV_LOG_DEBUG, "success to open audio device(%s)\n", m_deviceName.c_str());
     }
+
 }
 
 Device::~Device()
 {
-    avformat_close_input(&m_fmtCtx);
+    if (m_fmtCtx)
+        avformat_close_input(&m_fmtCtx);
 }
 
 void Device::record(const std::string& outFilename)
@@ -45,15 +50,75 @@ void Device::record(const std::string& outFilename)
     int recordCnt = 5000;
     AVPacket audioPacket;
     av_init_packet(&audioPacket);
-    int ret = 0;
-    ret = av_read_frame(m_fmtCtx, &audioPacket);
-    av_log(NULL, AV_LOG_DEBUG, "ret is %d", ret);
+
+    char *out = "audio.pcm";
+    FILE *outfile = fopen(out, "wb+");
+
+    // for resample
+    SwrContext* swrCtx = nullptr;
+    swrCtx = swr_alloc_set_opts(
+        nullptr, 
+        AV_CH_LAYOUT_STEREO, 
+        AV_SAMPLE_FMT_S16, 
+        44100, 
+        AV_CH_LAYOUT_STEREO, 
+        AV_SAMPLE_FMT_S16, 
+        44100, 
+        0, 
+        nullptr
+    );
+
+    if (!swrCtx)
+    {
+        av_log(NULL, AV_LOG_ERROR, "failed to alloct swr context.");
+        return;
+    }
+
+    if (swr_init(swrCtx) < 0)
+    {
+        av_log(NULL, AV_LOG_ERROR, "failed to init swr context.");
+        return;
+    }
+
+    uint8_t** srcData = nullptr;
+    int srcLineSize = 0;
+    // 64/2 = 32 32/2 = 16
+    av_samples_alloc_array_and_samples(&srcData, &srcLineSize, 1, 16, AV_SAMPLE_FMT_S16, 0);
+
+    uint8_t** dstData = nullptr;
+    int dstLineSize = 0;
+    av_samples_alloc_array_and_samples(&dstData, &dstLineSize, 1, 16, AV_SAMPLE_FMT_S16, 0);
 
     while (av_read_frame(m_fmtCtx, &audioPacket) == 0 && recordCnt > 0)
     {
-        av_log(NULL, AV_LOG_DEBUG, "audeo packet size : %d\n", audioPacket.size);
         recordCnt--;
-        ofs.write(reinterpret_cast<char*>(audioPacket.data), audioPacket.size);
+
+        memcpy(static_cast<void*>(srcData[0]), static_cast<void*>(audioPacket.data), audioPacket.size);
+
+        int ret = swr_convert(swrCtx,                    //重采样的上下文
+                    dstData,                   //输出结果缓冲区
+                    16,                        //每个通道的采样数
+                    (const uint8_t **)srcData, //输入缓冲区
+                    16);                       //输入单个通道的采样数
+
+        av_log(NULL, AV_LOG_DEBUG, "audio packet size : %d dstLineSize: %d srcLineSize %d\n ret %d\n", audioPacket.size, dstLineSize, srcLineSize, ret);
+
+        ofs.write(reinterpret_cast<char*>(dstData[0]), dstLineSize);
+        fwrite(dstData[0], 1, dstLineSize, outfile);
+        fflush(outfile);
         av_packet_unref(&audioPacket);
     }
+
+    if (srcData)
+    {
+        av_freep(&srcData[0]);
+    }
+    av_free(srcData);
+    if (dstData)
+    {
+        av_freep(&dstData[0]);
+    }
+    av_free(dstData);
+
+    swr_free(&swrCtx);
 }
